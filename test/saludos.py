fa69bc5d91d1c2f5c5b8f0f5ba90ab7c1879694b
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-09 15:21:00 +0000

    mm

---
+++ test/saludos.py
@@ -0,0 +1,6 @@
+class saludos:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    
+    def saludar(self):
+        print('Hola, soy: ' + self.nombre)
---
+++ test/script.py
@@ -0,0 +1,4 @@
+from test.saludos import saludos
+
+s = saludos('Felix')
+s.saludar()
