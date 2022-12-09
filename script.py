c898194562188bf90763419b4d6642a8c1e932ec
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-09 15:06:25 +0000

    ave maria cuando seras mia

---
+++ script.py
@@ -0,0 +1,4 @@
+from saludos import saludar #Al poner esto en vez del import total, ocupamos menos espacio de memoria
+#ver por que no funciona
+
+
---
+++ slaudos.py
@@ -0,0 +1,4 @@
+def saludar():
+    print('Holi, saludo')
+
+print(saludar)
